a84bb10db9da722c61f9a78507109111cb2e0138
kassidyb816 <kassidy.barnes65@gmail.com>
2020-12-24 22:18:22 -0600

    Push JS
    I am just here to push code

--- main.js
+++ main.js
@@ -1,1 +1,53 @@
-main.js
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+    
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined 
+
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+
+    }
+
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operaton]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperatorandTextElement = document.querySelector('[data-current-operand]')
+    
+const calculator = new Calculator(previousOperandandTextElement, currentOperatorandTextElement)
+
+numberButtons.forEach(button => {
+   button.addEventListener('click', () => {
+       calculator.appendNumber(button.innerText)
+       calculator.updateDisplay()
+   })
+})
